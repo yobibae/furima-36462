b8bec7a3ee2c14dd136949676c0cc7c5ed56b500
yobibae <bj708012@gmail.com>
2021-11-23 23:26:12 +0900

    userモデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname { 'furima' }
+    email { 'furima@gmail.com' }
+    password { 'f11111' }
+    password_confirmation { password }
+    last_name { 'フリマ' }
+    first_name { 'フリマ' }
+    last_name_kana { 'フリマ' }
+    first_name_kana { 'フリマ' }
+    birth_date {'1930-01-01'}
+  end
+end
