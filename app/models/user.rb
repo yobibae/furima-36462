db164cc98f612faf74c127238e38e7f055d84803
yobibae <bj708012@gmail.com>
2021-11-24 00:22:35 +0900

    バリデーション, allow_blank: true追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,14 +8,14 @@ class User < ApplicationRecord
   # has_many :orders
 
   validates :nickname, presence: true
-  validates :email, presence: true
+  validates :email, presence: true, allow_blank: true
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }, allow_blank: true
   HIRAGANA_KATAKANA_KANJI_REGEXP = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
-  validates :last_name, presence: true, format: { with: HIRAGANA_KATAKANA_KANJI_REGEXP }
-  validates :first_name, presence: true, format: { with: HIRAGANA_KATAKANA_KANJI_REGEXP }
+  validates :last_name, presence: true, format: { with: HIRAGANA_KATAKANA_KANJI_REGEXP, allow_blank: true }
+  validates :first_name, presence: true, format: { with: HIRAGANA_KATAKANA_KANJI_REGEXP, allow_blank: true }
   KATAKANA_REGEXP = /\A[ァ-ヶー]+\z/
-  validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
-  validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP, allow_blank: true }
+  validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP, allow_blank: true }
   validates :birth_date, presence: true
 end
