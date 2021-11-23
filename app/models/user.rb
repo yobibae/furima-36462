733426a6f152683e8b86f12d400b52b65eaa8217
yobibae <bj708012@gmail.com>
2021-11-23 20:34:20 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+              
+  validates :nickname, presence: true
+  validates :email, presence: true
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, presence: true, format: { with: VALID_PASSWORD_REGEX }
+  HIRAGANA_KATAKANA_KANJI_REGEXP = /\A[ぁ-んァ-ン一-龥]/
+  validates :last_name, presence: true, format: { with: HIRAGANA_KATAKANA_KANJI_REGEXP }
+  validates :first_name, presence: true, format: { with: HIRAGANA_KATAKANA_KANJI_REGEXP }
+  KATAKANA_REGEXP = /\A[ァ-ヶー－]+\z/
+  validates :last_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :first_name_kana, presence: true, format: { with: KATAKANA_REGEXP }
+  validates :birth_date, presence: true
+
 end
