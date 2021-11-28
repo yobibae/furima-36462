cdbef34225b1bc73fb0b60ae44753c4d0a5d3b97
yobibae <bj708012@gmail.com>
2021-11-28 15:40:05 +0900

    itemsモデルアソシエーション

---
+++ app/models/item.rb
@@ -0,0 +1,14 @@
+class Item < ApplicationRecord
+  belongs_to :user
+  # has_one_attached :image
+  # has_one :order
+
+  # validates :image, presence: true
+  validates :name, presence: true
+  validates :info, presence: true
+  validates :category_id, presence: true
+  validates :sales_status_id, presence: true
+  validates :item_shipping_fee_status_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :item_scheduled_delivery_id, presence: true
+end
