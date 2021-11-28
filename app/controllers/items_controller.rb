46f4c73b04ee8351108cd3b0d74b306f6be15c5d
yobibae <bj708012@gmail.com>
2021-11-28 23:32:44 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
   def index
+    @item = Item.order("created_at DESC")
   end
 
   def new
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,10 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :sales_status
+  belongs_to :item_shipping_fee_status
+  belongs_to :prefecture
+  belongs_to :item_scheduled_delivery
 
   belongs_to :user
   has_one_attached :image
@@ -9,9 +13,19 @@ class Item < ApplicationRecord
   validates :image, presence: true
   validates :name, presence: true
   validates :info, presence: true
-  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
-  validates :sales_status_id, presence: true
-  validates :item_shipping_fee_status_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :item_scheduled_delivery_id, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :sales_status_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_shipping_fee_status_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :item_scheduled_delivery_id, numericality: { other_than: 1 , message: "can't be blank" }
+  VALID_PRICEL_REGEX = /\A[0-9]+\z/
+  validates :price,
+             presence: { message: "can't be blank" },
+             format: { with: VALID_PRICEL_REGEX, allow_blank: true },
+             numericality: {
+                            only_integer: true,
+                            greater_than_or_equal_to: 300,
+                            less_than_or_equal_to: 9999999,
+                            allow_blank: true
+                           }
 end
