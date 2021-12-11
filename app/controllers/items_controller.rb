1be2a1dff5927d9ec5cf349140ed0882c08d0972
yobibae <bj708012@gmail.com>
2021-12-11 22:00:59 +0900

    editアクション条件分岐

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,6 +24,9 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
   end
 
   def update
