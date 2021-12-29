4965fff215ceeb35099768dc7d3f048d34dd1e2d
yobibae <bj708012@gmail.com>
2021-12-29 13:12:26 +0900

    URL直打ち対策#2

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :authenticate_user!, except: :index
+  before_action :authenticate_user!, only: [:index, :create]
   
   def index
     @item = Item.find(params[:item_id])
